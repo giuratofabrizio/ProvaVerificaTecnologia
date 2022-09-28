9a32229a0c2ad685923558466574e6b7cae2308d
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2022-09-28 14:05:46 +0000

    punto 2/3

--- fabrizio-giurato/src/app/app.module.ts
+++ fabrizio-giurato/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
+import { Componente3Component } from './componente3/componente3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Componente2Component,
+    Componente3Component
   ],
   imports: [
     BrowserModule
